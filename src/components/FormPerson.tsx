4a8ee958da568651888ca0b087870de06e1719e3
joaogabrielfragosojardim <joaogabrielmgtsu@gmail.com>
2022-09-12 22:55:35 -0300

    removing all form validations

--- src/components/FormPerson.tsx
+++ src/components/FormPerson.tsx
@@ -1,16 +1,5 @@
-import {
-  Flex,
-  Avatar,
-  Input,
-  FormLabel,
-  Box,
-  Text,
-  Button,
-} from "@chakra-ui/react";
-import { ErrorMessage } from "@hookform/error-message";
-import { useCallback } from "react";
-import { useForm } from "react-hook-form";
-import { validatePhone } from "validations-br";
+import { Flex, Avatar, FormLabel, Box, Text } from "@chakra-ui/react";
+
 import { IData } from "../services/pessoa";
 
 interface IFormPeople {
@@ -20,22 +9,11 @@ interface IFormPeople {
 export const FormPerson = ({ dataPessoa }: IFormPeople) => {
   const textPhone = dataPessoa.phones.length > 1 ? "Telefones:" : "Telefone:";
   const textBirthday = new Date(dataPessoa.birthday).toLocaleDateString();
-  const required = "Preencha este campo";
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IData>();
-
-  const onSubmit = useCallback((data: IData) => {
-    console.log(data);
-  }, []);
 
   return (
     <Flex flexDir="column" gap="25px" align="center">
       <Avatar src="" size="2xl" />
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form>
         <Flex gap="25px" flexDir="column">
           <Flex gap="45px" justify="space-between">
             <Box>
@@ -60,9 +38,6 @@ export const FormPerson = ({ dataPessoa }: IFormPeople) => {
               </Box>
             </>
           ) : null}
-          <Button type="submit" bg="blue.300">
-            Editar
-          </Button>
         </Flex>
       </form>
     </Flex>
